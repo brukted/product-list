938480cfbc2fc6d4a87832ee98e664def08d3ad7
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-01 10:00:45 +0300

    fix env config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,9 @@
-export const MONGO_DB_CONNECTION_STRING = 'mongodb://localhost:27017';
-export const JWT_SECRET = 'secret';
+import { load } from "ts-dotenv";
+
+const env = load({
+    MONGO_DB_CONNECTION_STRING: { type: String, default: 'mongodb://localhost:27017' },
+    JWT_SECRET: { type: String, default: 'secret' },
+});
+
+export const MONGO_DB_CONNECTION_STRING = env.MONGO_DB_CONNECTION_STRING;
+export const JWT_SECRET = env.JWT_SECRET;
